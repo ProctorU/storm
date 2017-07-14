ca4ea1f0f80cbe2a9b0ff250a692e9cbcd0785f2
Andrew Fomera <andrew@zerlex.net>
2017-07-14 11:01:38 -0500

    whoops, validate only before creation

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -8,11 +8,11 @@ class Setting < ApplicationRecord
     key: Rails.application.secrets.secret_key_base.byteslice(0, 32)
   )
 
-  validate :only_one_setting_record
+  before_create :validate_only_one_setting_record
 
   private
 
-  def only_one_setting_record
+  def validate_only_one_setting_record
     errors.add(:base, 'There can only be one setting') if Setting.count > 0
   end
 end
