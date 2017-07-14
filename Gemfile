37ae6adc10320e1381d99ce1088d89b3b770e107
Andrew Fomera <andrew@zerlex.net>
2017-07-14 16:20:46 -0500

    Add slack notifier and service class to ping

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'rails', '~> 5.1.1'
 gem 'sass-rails', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
+gem 'slack-notifier', '~> 2.2.2'
 gem 'webpacker', '~> 2.0'
 
 group :test do
---
+++ app/services/notifiers/slack_notifier.rb
@@ -0,0 +1,27 @@
+module Notifiers
+  class SlackNotifier
+    attr_reader :message, :bot, :notifier, :bot_name
+
+    def initialize(message, bot = nil)
+      @message = message
+      @bot = bot
+      @notifier = Slack::Notifier.new(Setting.first.slack_url) do
+        defaults username: @bot_name
+      end
+    end
+
+    def notify!
+      notifier.ping(message, icon_url: icon_url)
+    end
+
+    private
+
+    def bot_name
+      bot.present? ? bot : 'storm'
+    end
+
+    def icon_url
+      'https://s3-us-west-2.amazonaws.com/dev-team-resources/storm-icon.png'
+    end
+  end
+end
