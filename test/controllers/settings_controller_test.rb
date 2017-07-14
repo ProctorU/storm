4a2ae08c5ee5b9a363bebb0d53a1e9ccf750bc96
Andrew Fomera <andrew@zerlex.net>
2017-07-14 13:00:46 -0500

    update settings test syntax

--- test/controllers/settings_controller_test.rb
+++ test/controllers/settings_controller_test.rb
@@ -1,13 +1,22 @@
 require 'test_helper'
 
 class SettingsControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @user = create(:user)
-    sign_in @user
+  feature 'as as authenticated user' do
+    before(:each) do
+      @user = create(:user)
+      sign_in(@user)
+    end
+
+    test 'should get edit settings' do
+      get(edit_settings_url)
+      assert_response(:success)
+    end
   end
 
-  test "should get edit" do
-    get edit_settings_path
-    assert_response :success
+  feature 'as as an unauthenticated user' do
+    test 'should get redirected to sign-in' do
+      get(users_url)
+      assert_redirected_to(new_user_session_path)
+    end
   end
 end
