7e80046e08d1370ee3eaf5b013632076b4c7e19a
Justin Licata <licata.justin@gmail.com>
2018-01-14 11:37:29 -0600

    Remove custom `created_at` attribute

--- app/serializers/ping_serializer.rb
+++ app/serializers/ping_serializer.rb
@@ -2,8 +2,4 @@ class PingSerializer < ActiveModel::Serializer
   attributes(
     :id, :status, :created_at, :status_badge, :status_text
   )
-
-  def created_at
-    object.created_at.strftime('%B %e, %Y at %l:%M%P')
-  end
 end
