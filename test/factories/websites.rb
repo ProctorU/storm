790ad2b6386d410eb73d9b9a69e118a5a6996d68
Andrew Fomera <andrew@zerlex.net>
2019-02-11 20:24:47 -0600

    Fix FactoryBot deprecations

--- test/factories/websites.rb
+++ test/factories/websites.rb
@@ -2,17 +2,17 @@ FactoryBot.define do
   factory :website do
     sequence(:name) { |n| "Website #{n}" }
     sequence(:url) { |n| "http://#{n}.proctoru.com" }
-    ssl(false)
-    active(true)
-    rebooting(false)
+    ssl { false }
+    active { true }
+    rebooting { false }
 
     trait(:with_basic_auth) do
-      basic_auth_username('username')
-      basic_auth_password('password')
+      basic_auth_username {'username' }
+      basic_auth_password { 'password' }
     end
 
     trait(:inactive) do
-      active(false)
+      active { false }
     end
 
     factory :website_with_ping do
